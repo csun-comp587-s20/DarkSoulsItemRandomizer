5697cd4b143889f8017366b9a14c2eb3f0cc63fa
Kevin <seniorpepex@gmail.com>
2020-05-17 00:01:54 -0700

    Write DCX Test

--- tests/test_chr_init_param.py
+++ tests/test_chr_init_param.py
@@ -1,9 +1,6 @@
 import chr_init_param
 import unittest
 
-<<<<<<< HEAD
-# 
-=======
 # Extract shift jisz unit tests
 
 class extract_shift_jisz_unit_tests(unittest.TestCase):
@@ -24,7 +21,6 @@ class find_nonexistent_char():
     def runTest(self):
         assert self.find_chr_by_id_instance('B') == False, \
             "chr id's should be numbers"
->>>>>>> 5e66989e7192c1748e0eeea998f87d1410ae9666
 
 if __name__ == "__main__":
     unittest.main()
--- tests/test_dcx_handler.py
+++ tests/test_dcx_handler.py
@@ -1,5 +1,7 @@
 import dcx_handler
 import unittest
+from dcx_handler import compress_dcx_content
+from dcx_handler import uncompress_dcx_content
 
 # Comsume byte unit tests
 
@@ -42,6 +44,32 @@ class is_normal_array(appears_dcx_unit_tests):
     def runTest(self):
         assert self.appears_dcx_instance([-1, 2, 5, -8, 7]) == False, \
             "This is an instance of dcx"
+
+
+#compress/decompress unit tests
+class compress_dcx_content_unit_tests(unittest.TestCase):
+    def setUp(self):
+        self.compress_dcx_instance = dcx_handler.compress_dcx_content
+    def tearDown(self):
+        return super().tearDown()
+
+class uncompress_dcx_content_unit_tests(unittest.TestCase):
+    def setUp(self):
+        self.uncompress_dcx_instance = dcx_handler.uncompress_dcx_content
+    def tearDown(self):
+        return super().tearDown()
+
+class uncompress_test(uncompress_dcx_content_unit_tests):
+    def runTest(self):
+        comp = compress_dcx_content(b'MEMES')
+        assert self.uncompress_dcx_instance(comp) == b'MEMES', \
+            "uncompression pass"
+
+# class compress_test(compress_dcx_content_unit_tests):
+#     def runTest(self):
+#         uncomp = uncompress_dcx_content(b'MEMESMEMES')
+#         assert self.compress_dcx_instance(uncomp) == b'MEMES', \
+#             "compression pass"
 
 if __name__ == "__main__":
     unittest.main()
